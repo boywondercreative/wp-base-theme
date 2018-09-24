c78fbbd106b1ec3acb7b40f329aa83495fc53423
Thomas Verleye <thomasverleye@live.be>
2018-09-24 17:17:33 +0200

    csp: prevent errors when logged in

--- lib/config/csp.php
+++ lib/config/csp.php
@@ -5,6 +5,12 @@ add_filter(
 		$csp->upgrade_insecure_requests();
 		$csp->support_google_tag_manager();
 
+		if ( is_user_logged_in() ) {
+			$csp->add_img_src( 'https://secure.gravatar.com' );
+			$csp->add_font_src( 'data:' );
+			$csp->add_img_src( 'data:' );
+		}
+
 		return $csp;
 	}, 10, 1
 );
